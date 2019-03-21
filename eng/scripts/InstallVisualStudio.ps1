6440e3ea4f33d9295b0433595626ffde7b3ac99e
Doug Bunting <dougbu@microsoft.com>
2019-03-20 20:58:55 -0700

    Move to BYOC queues - aspnet/AspNetCore-Internal#2033 - use conditions matching aspnet/Extensions and dotnet/Arcade   - e.g. https://github.com/dotnet/arcade/blob/05cb24592af3989d8f274e68280a3c16520d2f71/azure-pipelines.yml#L49-L54 - install necessary VS 2019 components on CI - add `-Quiet` option to InstallVisualStudio.ps1 - revert part of 85ae18c723 because multiple editions of VS 2019 are now publicly available   - support BuildTools edition of VS 2019

--- eng/scripts/InstallVisualStudio.ps1
+++ eng/scripts/InstallVisualStudio.ps1
@@ -3,46 +3,38 @@
     Installs or updates Visual Studio on a local developer machine.
 .DESCRIPTION
     This installs Visual Studio along with all the workloads required to contribute to this repository.
-
+.PARAMETER Edition
+    Selects which 'offering' of Visual Studio to install. Must be one of these values:
+        BuildTools
+        Community
+        Professional
+        Enterprise (the default)
 .PARAMETER InstallPath
-    The location of Visual Studio
+    The location on disk where Visual Studio should be installed or updated.
 .PARAMETER Passive
     Run the installer without requiring interaction.
+.PARAMETER Quiet
+    Run the installer without UI and wait for installation to complete.
 .LINK
     https://visualstudio.com
     https://github.com/aspnet/AspNetCore/blob/master/docs/BuildFromSource.md
 .EXAMPLE
-    To install VS 2019 Preview, run this command in PowerShell:
+    To install VS 2019 Enterprise, run this command in PowerShell:
 
         .\InstallVisualStudio.ps1
 #>
 [CmdletBinding(DefaultParameterSetName = 'Default')]
 param(
-    # TODO - once VS 2019 16.0 RTM is released, make this a parameter again
-    # .PARAMETER Edition
-    # Must be one of these values:
-
-    # Community
-    # Professional
-    # Enterprise
-
-    # Selects which 'offering' of Visual Studio to install.
-
-    # [ValidateSet('Community', 'Professional', 'Enterprise')]
-    # [string]$Edition,
+    [ValidateSet('BuildTools','Community', 'Professional', 'Enterprise')]
+    [string]$Edition = 'Enterprise',
     [string]$InstallPath,
-    [switch]$Passive
+    [switch]$Passive,
+    [switch]$Quiet
 )
 
-# VS previews are only available publicly as 'Enterprise' versions. They should be available to the community to use without a paid license.
-$Edition = 'Enterprise'
-
-if (-not $Edition) {
-    Write-Host "You must specify a value for the -Edition parameter which selects the kind of Visual Studio to install." -f Red
+if ($Passive -and $Quiet) {
+    Write-Host "The -Passive and -Quiet options cannot be used together." -f Red
     Write-Host "Run ``Get-Help $PSCommandPath`` for more details." -f Red
-    Write-Host ""
-    Write-Host "Example:  ./InstallVisualStudio -Edition Community" -f Red
-    Write-Host ""
     exit 1
 }
 
@@ -54,11 +46,15 @@ mkdir $intermedateDir -ErrorAction Ignore | Out-Null
 
 $bootstrapper = "$intermedateDir\vsinstaller.exe"
 $ProgressPreference = 'SilentlyContinue' # Workaround PowerShell/PowerShell#2138
-Invoke-WebRequest -Uri "https://aka.ms/vs/16/pre/vs_$($Edition.ToLowerInvariant()).exe" -OutFile $bootstrapper
+Invoke-WebRequest -Uri "https://aka.ms/vs/16/release/vs_$($Edition.ToLowerInvariant()).exe" -OutFile $bootstrapper
 
 if (-not $InstallPath) {
-    # $InstallPath = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\$Edition"
-    $InstallPath = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\Preview"
+    $basePath = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2019\"
+    if ((Test-Path "$basePath\Preview") -and ("$Edition" -eq "Enterprise")) {
+        $InstallPath = "$basePath\Preview"
+    } else {
+        $InstallPath = "$basePath\$Edition"
+    }
 }
 
 # no backslashes - this breaks the installer
@@ -70,14 +66,22 @@ if (Test-path $InstallPath) {
     $arguments += 'modify'
 }
 
+$responseFile = "$PSScriptRoot\vs.json"
+if ("$Edition" -eq "BuildTools") {
+    $responseFile = "$PSScriptRoot\vs.buildtools.json"
+}
+
 $arguments += `
     '--productId', "Microsoft.VisualStudio.Product.$Edition", `
     '--installPath', "`"$InstallPath`"", `
-    '--in', "$PSScriptRoot\vs.json", `
+    '--in', "`"$responseFile`"", `
     '--norestart'
 
 if ($Passive) {
     $arguments += '--passive'
+}
+if ($Quiet) {
+    $arguments += '--quiet', '--wait'
 }
 
 Write-Host ""
@@ -85,4 +89,12 @@ Write-Host "Installing Visual Studio 2019 $Edition" -f Magenta
 Write-Host ""
 Write-Host "Running '$bootstrapper $arguments'"
 
-& $bootstrapper @arguments
+$process = Start-Process -FilePath "$bootstrapper" -ArgumentList $arguments `
+    -PassThru -RedirectStandardError "$intermedateDir\errors.txt" -Verbose -Wait
+if ($process.ExitCode -ne 0) {
+    Get-Content "$intermedateDir\errors.txt" | Write-Error
+}
+
+Remove-Item "$intermedateDir\errors.txt" -errorAction SilentlyContinue
+
+exit $process.ExitCode
